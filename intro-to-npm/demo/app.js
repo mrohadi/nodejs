ab8bc2a6993f6cd40ff957402437f366f8a62908
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-07-27 15:05:25 +0700

    intro to npm

---
+++ intro-to-npm/demo/app.js
@@ -0,0 +1,8 @@
+// import the packages
+const catMe = require("cat-me");
+const jokes = require("knock-knock-jokes");
+
+// Using catMe package
+console.log(catMe());
+// Using  knock-knock-jokes
+console.log(jokes());
